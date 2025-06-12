bceaf0284a805f7e1ec6cd761f3588235f9c3fd8
Ace-Aryal <aryaldeelep@gmail.com>
2025-06-12 20:48:16 +0545

    add head.tsx

---
+++ portfolio-next/src/app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Your Site Title</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="portfolio website "
+        content="new and updated portfolio website using next.js and mdx"
+      />
+    </>
+  )
+}
