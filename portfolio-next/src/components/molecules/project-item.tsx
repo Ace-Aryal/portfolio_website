da02843f6e0539ed1216db875a83b961b348e333
Dipesh Aryal <aryaldipesh404@gmail.com>
2025-06-17 15:00:24 +0545

    adds general accessibility

--- portfolio-next/src/components/molecules/project-item.tsx
+++ portfolio-next/src/components/molecules/project-item.tsx
@@ -5,8 +5,11 @@ import Link from 'next/link'
 import { formatDate } from '@/lib/formatDate'
 import { motion } from 'motion/react'
 import { fadeInAnimationVariants } from '@/lib/framer-motion'
+import { twMerge } from 'tailwind-merge'
+import { useState } from 'react'
 
 function ProjectCard({ project, index }: { project: projectMetaData; index: number }) {
+  const [isHovernig, setIsHovering] = useState(false)
   return (
     <motion.div
       variants={fadeInAnimationVariants}
@@ -16,11 +19,18 @@ function ProjectCard({ project, index }: { project: projectMetaData; index: numb
         once: true,
       }}
       custom={index}
-      className="h-full w-full rounded-lg border-0 hover:border-4 hover:border-gray-500 hover:transition-all focus:border-4 active:border-4"
+      onTouchStart={() => setIsHovering(true)}
+      onTouchEnd={() => setIsHovering(false)}
+      className="h-full w-full rounded-lg border-0 border-gray-500 hover:border-4 hover:transition-all focus:border-4 active:border-4"
     >
       <Link href={`/projects/${project.slug}`} className="aspect-square min-h-48 w-full rounded-lg">
         <div className="group relative aspect-square h-full w-full rounded-lg hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-100 hover:dark:bg-gray-800 focus:dark:bg-gray-800 active:dark:bg-gray-800">
-          <div className="absolute h-full w-full group-hover:opacity-25 group-focus:opacity-25 group-active:opacity-25">
+          <div
+            className={twMerge(
+              `absolute h-full w-full group-hover:opacity-25 group-focus:opacity-25 group-active:opacity-25`,
+              isHovernig ? 'opacity-25' : ''
+            )}
+          >
             <Image
               fill
               alt={project?.title || 'project image'}
@@ -31,7 +41,12 @@ function ProjectCard({ project, index }: { project: projectMetaData; index: numb
               className="rounded-lg object-cover"
             />
           </div>
-          <div className="absolute bottom-0 hidden p-1 text-zinc-900 group-hover:block group-focus:block group-active:block dark:text-white">
+          <div
+            className={twMerge(
+              `absolute bottom-0 hidden p-1 text-zinc-900 group-hover:block group-focus:block group-active:block dark:text-white`,
+              isHovernig ? 'block' : ''
+            )}
+          >
             <div className="text-md col-span-4 text-lg font-semibold">{project.title}</div>
             <div className="col-span-2 row-span-2 text-right text-sm">
               {formatDate(project.date)}
